26b89c03ea2d8ecd65f5f737f5e9c3b469d695d6
bmeko <48912701+bmeko@users.noreply.github.com>
2022-06-26 00:01:43 +0300

    Update file_auth.sol

--- contracts/file_auth.sol
+++ contracts/file_auth.sol
@@ -6,17 +6,50 @@ contract file_auth {
     struct File_in{
         uint id;
         string content;
+        string file_name;
+        string last_update;
+        string location;
         bool allowed;
     }
 
     mapping(uint => File_in) public tasks;
+    
+    event Fileadded(
+        uint id,
+        string content,
+        string file_name,
+        string last_update,
+        string location,
+        bool completed
+    );
+    event Fileallowed(
+        uint id,
+        bool allowed
+    );
     constructor() public {
-        createtask("onefile");
+        //createtask("onefile");
     }
-    function createtask(string memory _content) public {
+    function createtask(string memory _content,string memory _file_name, string memory _last_update,string memory location) public {
         taskcount ++;
-        tasks[taskcount]= File_in(taskcount,_content, false);
-        
+        tasks[taskcount]= File_in(taskcount,_content,_file_name,_last_update,location,false);
+        emit Fileadded(taskcount, _content,_file_name,_last_update,location, false);
     }
-
+    function allow(uint _id, bool role) public{
+       if (role){
+            File_in memory _file = tasks[_id];
+            _file.allowed = !_file.allowed;
+            tasks[_id]= _file;
+            emit Fileallowed(_id, _file.allowed);
+       }
+    }
+    function check(string memory _content) public view returns(bool){
+        uint i;
+        
+        for (i=0; i<taskcount; i++){
+            File_in memory _file = tasks[i];
+            if(keccak256(abi.encodePacked((_file.content))) == keccak256(abi.encodePacked((_content)))){
+                return true;
+            }
+        }
+    }
 }
